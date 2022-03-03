c9c7af30e7346f30d64d0c49dd1c5b58df3452b5
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2022-03-03 22:03:40 +0100

    product deleted

---
+++ src/pages/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Test = () => {  
+    return (
+      <Layout>
+        <p>Test page</p>
+      </Layout>
+    );
+  };
+
+  export default Test
